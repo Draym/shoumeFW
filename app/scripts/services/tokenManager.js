8359f61e1b453204069559526c8e701e86e93baa
andres_k <kevin.andres@epitech.eu>
2016-11-06 15:46:52 +0800

    add tokenManager correction width login css

---
+++ app/scripts/services/tokenManager.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name shoumeApp.tokenManager
+ * @description
+ * # tokenManager
+ * Factory in the shoumeApp.
+ */
+angular.module('shoumeApp')
+  .factory('tokenManager', function () {
+    // Service logic
+    // ...
+
+    // Public API here
+    return {
+      get: function () {
+      },
+      set: function(value) {
+      },
+      remove: function() {
+      },
+      init: function() {
+      }
+    };
+  });
