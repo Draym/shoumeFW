1230cfa0e2b38d2b4ca74aa93337d5c3b66a3c34
andres_k <kevin.andres@epitech.eu>
2016-11-13 14:32:19 +0800

    [CORRECT] create moment

--- app/scripts/controllers/moments/addMomentModal.js
+++ app/scripts/controllers/moments/addMomentModal.js
@@ -10,8 +10,9 @@
 angular.module('shoumeApp')
   .controller('AddMomentModalCtrl', function ($scope, $uibModalInstance, RequestAPI, SubmitResult, TokenManager) {
 
-    $scope.save = function() {
+    $scope.save = function () {
       RequestAPI.POST("/moment", $scope.moment, SubmitResult.submitSuccess(function (response) {
+          $scope.init();
           $scope.clear();
         }, "Moment created"),
         SubmitResult.submitFailure(), TokenManager.get());
